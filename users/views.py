4bf19327d01ac57935ba902e73a48e757105d450
hugogomess <hugogomes02@gmail.com>
2019-09-01 05:13:31 -0300

    add authentication by token

--- users/views.py
+++ users/views.py
@@ -1,4 +1,7 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+
 from .models import User
 from .serializers import UserSerializer
 
@@ -6,3 +9,5 @@ class UserViewSet(ModelViewSet):
 
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    permission_classes = (IsAuthenticated,)
+    authentication_classes = (TokenAuthentication,)
