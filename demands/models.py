4968c7d0199ee50fee08758afe17b450cea3ab4e
hugogomess <hugogomes02@gmail.com>
2019-11-06 19:35:36 -0300

    fix id, description, origin and created_at fields

--- demands/models.py
+++ demands/models.py
@@ -8,10 +8,10 @@ class Demand(models.Model):
         ('FECHADA', 'FECHADA'),
     )
 
-    name = models.CharField(blank=False, null=False, max_length=100)
-    description = models.TextField(blank=False, null=False)
+    name = models.CharField(blank=False, null=False, max_length=100, unique=True)
+    description = models.TextField(blank=False, null=False, max_length=5000)
     requester = models.CharField(blank=False, null=False, max_length=70)
-    origin = models.TextField(blank=False, null=False)
+    origin = models.TextField(blank=False, null=False, max_length=5000)
     status = models.CharField(choices=STATUS, null=False, max_length=20, default='ABERTA')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
