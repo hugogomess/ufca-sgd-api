0e4cc5259fb35c5a3ad405a7f50663a4d776a1c6
Gustavo Siqueira <gustavosiqueira.97@gmail.com>
2019-12-04 18:05:35 -0300

    test users

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from models import User
+from rest_framework.test import APIRequestFactory
+from rest_framework.test import force_authenticate 
+from rest_framework.test import APIClient 
+from rest_framework.test import APIRequestFactory 
+from django.test.client import encode_multipart, RequestFactory
 
-# Create your tests here.
+factory = RequestFactory()
+data = {'title': 'remember to email dave'}
+content = encode_multipart('BoUnDaRyStRiNg', data)
+content_type = 'multipart/form-data; boundary=BoUnDaRyStRiNg'
+request = factory.put('/notes/547/', content, content_type=content_type)
+
+client = APIClient()
+client.login(username='admin', password='admin')
+
+user = User.objects.get(username='')
+request = factory.get('/accounts/django-superstars/')
+force_authenticate(request, user=user, token=user.auth_token)
